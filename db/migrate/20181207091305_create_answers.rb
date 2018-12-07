3c9bacc4af2e2c806a278de1406e5f8b0efc0835
Unknown <spartakus1688@gmail.com>
2018-12-07 12:19:38 +0300

    Add has_many associations question to answers

--- db/migrate/20181207091305_create_answers.rb
+++ db/migrate/20181207091305_create_answers.rb
@@ -2,8 +2,8 @@ class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
       t.text :body
-      t.t.boolean :correct
-      t.t.integer :question_id
+      t.boolean :correct
+      t.integer :question_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_07_074905) do
+ActiveRecord::Schema.define(version: 2018_12_07_091305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.boolean "correct"
+    t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
