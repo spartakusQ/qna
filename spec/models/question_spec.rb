8d7102141e0ca9b6f23db4c4fa8a8ea538ede635
Unknown <spartakus1688@gmail.com>
2018-12-07 11:12:14 +0300

    New test with shoulda matchers rules

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it 'validates presence of title' do
-    question = Question.new(body: '123')
-    expect(question).to_not be_valid
-  end
-
-  it 'validates presence of body' do
-    question = Question.new(title: '123')
-    expect(question).to_not be_valid
-  end
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
 end
