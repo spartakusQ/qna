5ae57999bc670e998b7aec087316f94b6b24da76
Unknown <spartakus1688@gmail.com>
2018-12-11 15:40:06 +0300

    Complete test for #create (answer controller)

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,4 +5,15 @@ class AnswersController < ApplicationController
     @answer = @question.answers.new
   end
 
+
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new(params.require(:answer).permit(:body, :question_id))
+    
+    if @answer.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -7,13 +7,37 @@ RSpec.describe AnswersController, type: :controller do
   describe 'GET #new' do
     before { get :new, params: { question_id: question } }
 
-      it 'adding new answer' do
-        expect(assigns(:answer)).to be_a_new(Answer)
+    it 'adding new answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'render new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'save new answer in the database' do
+        expect { post :create, params: { answer: attributes_for(:answer), question_id: question } }.to change(Answer, :count).by(1)
       end
 
-      it 'render new view' do
+      it 'redirect to question show view' do
+        post :create, params: { answer: attributes_for(:answer), question_id: question }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'not save the answer' do
+        expect { post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question } }.to_not change(Answer, :count)
+      end
+
+      it 'render new view path' do
+        post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question }
         expect(response).to render_template :new
       end
     end
+  end
 
 end
