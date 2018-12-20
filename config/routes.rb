2e19c11eb060e500829a7903fb3103fe3df6c469
Unknown <spartakus1688@gmail.com>
2018-12-20 18:36:55 +0300

    Add route & migration on best answer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :questions, shallow: true do
-    resources :answers, only: [:create, :edit, :update, :destroy]
+    resources :answers, only: [:create, :edit, :update, :destroy] do
+      patch :best, on: :member
+    end
   end
 
   root to: "questions#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_13_101225) do
+ActiveRecord::Schema.define(version: 2018_12_20_153107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_12_13_101225) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.boolean "best", default: false
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
