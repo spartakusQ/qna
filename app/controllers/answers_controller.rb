73098d261077e7affc011fb8b865a49ba7f29111
Unknown <spartakus1688@gmail.com>
2019-01-10 23:07:52 +0300

    User can add link to answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -40,7 +40,7 @@ class AnswersController < ApplicationController
   end
 
   def answers_params
-    params.require(:answer).permit(:body, files: [])
+    params.require(:answer).permit(:body, files: [], links_attributes: [:name, :url])
   end
 
   def find_answer
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,11 +3,15 @@ class Answer < ApplicationRecord
 
   belongs_to :question
   belongs_to :user
+
   has_many_attached :files, dependent: :destroy
+  has_many :links, dependent: :destroy, as: :linkable
+
 
   validates :body, presence: true
   validate :best_count, on: :best
 
+  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   default_scope -> { order('best DESC, created_at') }
   scope :best, -> { where(best: true) }
